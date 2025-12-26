f4cd8caf3b28178a3edbfc356d767488a7817357
AVENGERSARSBRAND <170395208+KGFBRAND792@users.noreply.github.com>
2025-12-26 17:45:33 +0530

    Update and rename server .js to server.js

--- server.js
+++ server.js
@@ -1752,4 +1752,5 @@ def stop_sending():
     return jsonify({'success': True, 'message': 'Stopped sending messages'})
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000, debug=False)
